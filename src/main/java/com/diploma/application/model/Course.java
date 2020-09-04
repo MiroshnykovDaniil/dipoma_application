633737ececa217787017fabe49d5bde1304b56b8
Daniil Miroshnykov <danielq911q@gmail.com>
2020-09-04 14:42:21 +0300

    Course entity class

---
+++ src/main/java/com/diploma/application/model/Course.java
@@ -0,0 +1,33 @@
+package com.diploma.application.model;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @NotBlank(message = "course title cannot be empty or consist of spaces")
+    private String title;
+
+    @NotBlank(message = "course description cannot be empty or consist of spaces")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id")
+    @NotNull
+    private User creator;
+
+}
