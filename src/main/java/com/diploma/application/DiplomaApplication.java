371f352eb6ccd2d3d5ef699a47e5ecb2dbc4e1cc
Daniil Miroshnykov <danielq911q@gmail.com>
2021-02-19 13:09:10 +0200

    update for tomcat

--- src/main/java/com/diploma/application/DiplomaApplication.java
+++ src/main/java/com/diploma/application/DiplomaApplication.java
@@ -4,10 +4,11 @@ import com.diploma.application.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({AppProperties.class})
-public class DiplomaApplication {
+public class DiplomaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DiplomaApplication.class, args);
