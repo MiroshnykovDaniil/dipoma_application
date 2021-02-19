4efc9e1f11bb315b72a9a4e47950cc8d26047c20
Daniil Miroshnykov <danielq911q@gmail.com>
2021-02-19 14:52:45 +0200

    missed repository annotation

--- src/main/java/com/diploma/application/repository/UserProjectionRepository.java
+++ src/main/java/com/diploma/application/repository/UserProjectionRepository.java
@@ -3,7 +3,9 @@ package com.diploma.application.repository;
 import com.diploma.application.model.User;
 import com.diploma.application.projection.user.UserOnlyInfoProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserProjectionRepository extends JpaRepository<User,String> {
 
     UserOnlyInfoProjection getDistinctTopById(String id);
